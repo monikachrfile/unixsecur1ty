391fddb8d6d7d3f651e57a0aa0de7b3758a60a50
Diego Cruz <57609866+Kirito-Kun07@users.noreply.github.com>
2023-10-12 17:26:41 -0700

    Delete script_permissions.sh

--- script_permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod +x Harden_Defensive_Countermeasures.sh
-chmod +x Remove_Bad_Programs.sh 
-chmod +x User_Account_Configurations.sh
-chmod +x The_Last_Stand...sh
-chmod +x Manage_Services.sh
