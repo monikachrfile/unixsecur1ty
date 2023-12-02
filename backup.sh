38017b65aab68e52418a288d9f83eb813f44aa56
Diego Cruz <57609866+Kirito-Kun07@users.noreply.github.com>
2023-12-02 01:39:37 -0800

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+mkdir .backups
+chmod 770 .backups/
+mkdir .backups/important/
+mkdir .backups/web/
+mkdir .backups/logs/
+mkdir .backups/home/
+chmod 770 -R .backups/
+cp /etc/shadow* .backups/important/
+cp /etc/passwd* .backups/important/
+cp /etc/group .backups/important/
+cp /etc/group- .backups/important/
+cp -ar /etc/pam.d/ .backups/important/
+cp /etc/login.defs .backups/important/
+cp /etc/sysctl.conf .backups/important/
+cp /etc/cron.allow .backups/important/
+cp /etc/cron.deny .backups/important/
+cp /etc/at.deny .backups/important/
+cp -ar /etc/cron.d/ .backups/important/
+cp -ar /var/spool/cron/ .backups/important/
+cp /etc/sudoers .backups/important/
+cp -r /etc/sudoers.d/ .backups/important/
+cp /etc/login.defs .backups/important/
+cp -ar /etc/lightdm/ .backups/important/
+cp -ar /etc/ .backups/
+cp -ar /home/ .backups/home/
+cp -ar /var/www/ .backups/web/
+cp -ar /srv/ .backups/web/
+cp -ar /var/log/ .backups/log/
+cp ~/inventory.txt .backups/
+
+sudo apt install zip unzip -y
+dnf install zip -y
+sudo yum install zip
+sudo pacman -S zip
+apk add zip
+sudo pkg install zip
+
+zip blackteam.zip .backups/
